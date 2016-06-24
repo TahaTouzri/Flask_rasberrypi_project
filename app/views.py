2529c2ba6b0cd7e9fa1ab81dfddfc236847ae451
TahaTouzri <touzritaha@gmail.com>
2016-06-24 18:10:29 +0200

    add the @login_required decorator and fix few database issues

--- app/views.py
+++ app/views.py
@@ -1,14 +1,18 @@
 from gevent.pywsgi import WSGIServer
 from flask import Flask, json, Response, render_template,request,redirect
-from flask_login import LoginManager,login_user
+from flask_login import LoginManager,login_user,login_required
 from flask_wtf import validators,form
 import sqlite3
 from random import randint
 from time import sleep
 from jinja2 import Template
 
+# Configure authentication
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
 login_manager.init_app(app)
 
 #in the login branch
@@ -85,8 +89,9 @@ def load_user(id):
 	conn.close()
 	id = data[0]
 	name = data[1]
-	is_active = data[2]
-	is_authenticated = data[3]
+	is_authenticated = data[2]
+	#is_active = data[3]
+	is_active = 0
 	if is_active == 1:
 		is_active = True
 	else:
@@ -98,33 +103,39 @@ def load_user(id):
 	user= User(id,name,is_active,is_authenticated)
 	return user
 	
-@app.route('/login.html', methods=['GET', 'POST'])
+@app.route('/login.html', methods=['POST'])
 def login():
+	print "in the login view"
 	def next_is_valid(next):
-		return True
+		return False
 	# Here we use a class of some kind to represent and validate our
 	# client-side form data. For example, WTForms is a library that will
 	# handle this for us, and we use a custom LoginForm to validate.
 	#form = LoginForm()
-	print "in the login view"
+	form = request.form
+	print form
+	"""
 	if True:#form.validate_on_submit():
 		# Login and validate the user.
 		# user should be an instance of your `User` class
 		#login_user(user)
-		print "before get next"
-		print request
-		print request.args
 		next = request.args.get('next')
-		print "-----------------"
-		print next
 		# next_is_valid should check if the user has valid
 		# permission to access the `next` url
 		if not next_is_valid(next):
 			return flask.abort(400)
 		print next
 		print "redirect to after login"
-	return redirect('/welcome.html')
-	#return flask.render_template('login.html', form=form)
+	"""
+	user_name = form["username"]
+	user=User("2",user_name)
+	if user_name=="taha":
+		user.set_authenticated(True)
+		login_user(user)
+		print "user logged in successfully"
+		return redirect('/welcome.html')
+	print "redering login.html"
+	return render_template('login.html', form=form)
 
 @app.route('/')
 def first_login():
@@ -137,12 +148,14 @@ def first_login():
 @app.route('/room1.html')
 @app.route('/room2.html')
 @app.route('/kitchen.html')
+@login_required
 def room():
 	room = str(request.url_rule)[1:]
 	return render_template('room.html', room=room)
 
 @app.route('/menu.html')
 @app.route('/welcome.html')
+@login_required
 def pages():
 	rule = str(request.url_rule)
 	return render_template(rule[1:])
@@ -154,6 +167,7 @@ def event():
 		yield ev.encode()
 		sleep(1)
 @app.route('/stream/', methods=['GET', 'POST'])
+@login_required
 def stream():
 	return Response(event(), mimetype="text/event-stream")
 	
